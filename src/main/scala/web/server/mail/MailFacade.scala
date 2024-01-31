3e3ae044a57f9038d8bcbe713420d76772cbc16c
vmarc <marc@vmarc.be>
2024-01-31 11:04:36 +0100

    improve responsive styling

--- src/main/scala/web/server/mail/MailFacade.scala
+++ src/main/scala/web/server/mail/MailFacade.scala
@@ -1,9 +1,21 @@
 package web.server.mail
 
+import org.jsoup.Jsoup
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import play.twirl.api.Html
+import play.twirl.api.Template1
+import play.twirl.api.Template3
+import web.common.Templates
+import web.domain.ImageDimension
+import web.pages.Page
+import web.pages.ReactionOkPage
+import web.pages.ReservationNokPage
+import web.pages.ReservationOkPage
 import web.server.mail.domain.Reaction
 import web.server.mail.domain.Reservation
+import web.tools.SiteBuilderOptions
+import web.view.Images
 
 @Component
 class MailFacade(
@@ -12,36 +24,6 @@ class MailFacade(
 ) {
 
   private val log = LoggerFactory.getLogger(classOf[MailController])
-
-  def reservationNok(reservation: Reservation): String = {
-    """|<html>
-       |<body>
-       |<p>
-       |  FOUT: Uw reservatie is niet gelukt.
-       |</p>
-       |<p>
-       |  Ga terug naar de vorige pagina en zorg dat het codewoord juist ingevuld is.
-       |</p>
-       |<p>
-       |  <a href="javascript:history.back()">Terug</a>
-       |</p>
-       |</body>
-       |</html>
-    """.stripMargin
-
-//    val images: Images = new Images(
-//      SiteBuilderOptions(),
-//      Seq(
-//        "images/pinokkelijn4.jpg" -> ImageDimension("302", "50")
-//      ).toMap
-//    )
-//    val context = Map[String, Any](
-//      "root" -> ".",
-//      "images" -> images,
-//      "reservation" -> reservation
-//    )
-//    pageGenerator.build(context, "templates/reservation-nok.ssp")
-  }
 
   def reaction(reaction: Reaction): String = {
     log.info(s"reaction: $reaction")
@@ -61,51 +43,38 @@ class MailFacade(
   }
 
   def reactionOk(reaction: Reaction): String = {
-    """|<html>
-       |<body>
-       |<p>Bedankt voor uw reactie!!!</p>
-       |</body>
-       |</html>
-    """.stripMargin
-
-//    val images: Images = new Images(
-//      SiteBuilderOptions(),
-//      Seq(
-//        "images/pinokkelijn4.jpg" -> ImageDimension("302", "50")
-//      ).toMap
-//    )
-//    val context = Map[String, Any](
-//      "root" -> ".",
-//      "images" -> images,
-//      "reaction" -> reaction
-//    )
-//    pageGenerator.build(context, "templates/reaction-ok.ssp")
+    val page = ReactionOkPage(".", reaction)
+    val contentTemplate = Templates.getTemplate[Template1[ReactionOkPage, Html]]("html.reactionOk")
+    val contents = contentTemplate.render(page)
+    renderPage(page, contents)
   }
 
   def reservationOk(reservation: Reservation): String = {
-    """|<html>
-       |<body>
-       |<p>
-       |  Uw reservatie is verstuurd.
-       |</p>
-       |<p>
-       |  De kaarten zijn aan de kassa ter beschikking op de avond van de voorstelling en kunnen daar betaald worden.
-       |</p>
-       |</body>
-       |</html>
-    """.stripMargin
+    val page = ReservationOkPage(".")
+    val contentTemplate = Templates.getTemplate[Template1[ReservationOkPage, Html]]("html.reservationOk")
+    val contents = contentTemplate.render(page)
+    renderPage(page, contents)
+  }
 
-    //    val images: Images = new Images(
-    //      SiteBuilderOptions(),
-    //      Seq(
-    //        "images/pinokkelijn4.jpg" -> ImageDimension("302", "50")
-    //      ).toMap
-    //    )
-    //    val context = Map[String, Any](
-    //      "root" -> ".",
-    //      "images" -> images,
-    //      "reservation" -> reservation
-    //    )
-    //    pageGenerator.build(context, "templates/reservation-ok.ssp")
+  def reservationNok(reservation: Reservation): String = {
+    val page = ReservationNokPage(".")
+    val contentTemplate = Templates.getTemplate[Template1[ReservationNokPage, Html]]("html.reservationNok")
+    val contents = contentTemplate.render(page)
+    renderPage(page, contents)
+  }
+
+  private def renderPage(page: Page, contents: Html): String = {
+    val pageTemplate = Templates.getTemplate[Template3[web.pages.Page, Images, Html, Html]]("html.page")
+    val string = pageTemplate.render(page, images(), contents)
+    Jsoup.parse(string.toString()).html()
+  }
+
+  private def images(): Images = {
+    new Images(
+      SiteBuilderOptions(),
+      Seq(
+        "images/pinokkelijn4.jpg" -> ImageDimension("302", "50")
+      ).toMap
+    )
   }
 }
