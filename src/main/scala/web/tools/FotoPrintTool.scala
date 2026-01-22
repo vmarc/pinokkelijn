08e1b8d8a917e46d289c62610812960784f740fb
vmarc <marc@vmarc.be>
2026-01-22 17:16:44 +0100

    2025 production "Dwazen"

--- src/main/scala/web/tools/FotoPrintTool.scala
+++ src/main/scala/web/tools/FotoPrintTool.scala
@@ -3,19 +3,22 @@ package web.tools
 object FotoPrintTool {
 
   def main(args: Array[String]): Unit = {
-    val fotoElements = 1.to(172).map(id => String.format("%03d", id)).map { id =>
+    val fotoElements = 190.to(311).map(id => String.format("%03d", id)).map { id =>
       s"""
-         |<foto id="2017-$id" web="false" fotograaf="Marijke Bisschops">
+         |
+         |<foto id="2025-$id" web="false" fotograaf="Valerie Dufraing">
          |  <personen>
-         |    <naam>Michiel Peeters</naam>
+         |    <naam>Plien Kets</naam>
+         |    <naam>Jan Meeusen</naam>
+         |    <naam>Carine Bergmans</naam>
+         |    <naam>Herman Dufraing</naam>
+         |    <naam>Tineke Pockelé</naam>
+         |    <naam>Noëlla De Rop</naam>
          |    <naam>Gilberte Verbeeck</naam>
          |    <naam>Kristin Verbeeck</naam>
-         |    <naam>Mitte Scheldeman</naam>
-         |    <naam>Jarrick De Kock</naam>
-         |    <naam>Luc Brosens</naam>
-         |    <naam>Herman Dufraing</naam>
-         |    <naam>Jan Meeusen</naam>
-         |    <naam>Chris De Rijck</naam>
+         |    <naam>Jef Baetens</naam>
+         |    <naam>Kevin Van der Sommen</naam>
+         |    <naam>Bart Van Loenhout</naam>
          |  </personen>
          |</foto>
          """.stripMargin
